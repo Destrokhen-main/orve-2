4fc333060d930cac8cc4836a13a3e91e75817ca1
Destrokhen <armagiddon19@gmail.com>
2023-09-09 12:40:46 +0300

    fix: add mounterNode to exports

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./reactive/index";
 export * from "./utils/index";
 export { genUID } from "./helper/generation";
 export { randInt } from "./helper/random";
+export { mounterNode } from "./mounter/index";
 
 export default {
   Node,
