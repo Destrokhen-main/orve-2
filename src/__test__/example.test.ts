fb037c2821a41bb91c87be03133de419714931b3
Пысларь Владислав <vypyslar@mts.ru>
2023-11-02 14:19:52 +0300

    feat: simple example

---
+++ src/__test__/example.test.ts
@@ -0,0 +1,14 @@
+import { ref, computed } from "../index";
+
+describe("Тестовые кейсы", () => {
+  test('Проверка a + b = c', () => {
+    const a = ref(10);
+    const b = ref(20);
+    const c = computed(() => a?.value + b?.value, [a, b]);
+
+    expect(c?.value).toBe(30);
+
+    (a as any).value += 10;
+    expect(c?.value).toBe(40);
+  });
+});
