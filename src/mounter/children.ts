5ca97ab28d497576a0f6d00cbe5a5cd4d9920471
Destrokhen <armagiddon19@gmail.com>
2024-10-18 23:55:23 +0300

    feat: mounterRef simple mounted

--- src/mounter/children.ts
+++ src/mounter/children.ts
@@ -1,7 +1,18 @@
 import { mounterComponent } from ".";
 import { NodeOP } from "../parser/parser";
 import { TypeNode } from "../parser/type";
-import { createText, insert } from "./dom";
+import { ReactiveType } from "../reactive/type";
+import {
+  createComment,
+  createText,
+  insert,
+  replaceElement,
+  setText,
+} from "./dom";
+
+function isComment(el: any) {
+  return el.nodeType === 8;
+}
 
 export function mountedStatic(root: Element | null, tree: any) {
   const text = tree.value;
@@ -30,6 +41,36 @@ export function mountedHTML(root: Element | null, tree: any) {
   return node;
 }
 
+export function mountedRef(root: Element | null, item: any) {
+  console.log(item);
+
+  const value = item.value;
+
+  let startNode: any = null;
+
+  if (value === null || value === undefined || typeof value === "boolean") {
+    startNode = createComment("Ref");
+  }
+
+  if (typeof value === "string" || typeof value === "number") {
+    startNode = createText(value.toString());
+  }
+
+  item.$sub.subscribe((nexValue: any) => {
+    if (isComment(startNode)) {
+      const el = createText(nexValue.toString());
+      replaceElement(startNode, el);
+      startNode = el;
+    } else {
+      setText(startNode, nexValue.toString());
+    }
+  });
+
+  if (root && startNode !== null) {
+    insert(startNode, root);
+  }
+}
+
 export function mounterChildren(root: Element | null, tree: NodeOP) {
   if (tree.type === TypeNode.Component) {
     return mounterComponent(root, tree);
@@ -44,6 +85,10 @@ export function mounterChildren(root: Element | null, tree: NodeOP) {
   }
 
   if (tree.type === TypeNode.Reactive) {
-    //
+    const i = tree as any;
+
+    if (typeof i.value === "object" && i.value.type === ReactiveType.Ref) {
+      mountedRef(root, i.value);
+    }
   }
 }
